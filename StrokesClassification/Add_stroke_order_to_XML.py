a1a8d0305f4075aac6f6d65516a69597a79b8891
heermasterlp <heermasterlp@gmail.com>
2019-02-19 13:50:31 +0800

    add func: add stroke order to XML dataset

---
+++ StrokesClassification/Add_stroke_order_to_XML.py
@@ -0,0 +1,101 @@
+# coding: utf-8
+
+import os
+import shutil
+import xml.etree.ElementTree as ET
+from utils.Functions import prettyXml
+
+
+def add_stroke_order_to_XML():
+    type_path = '/Users/liupeng/Documents/Data/stroke_classification_dataset/types'
+    stroke_name = [f.replace('.png', '') for f in os.listdir(type_path) if '.png' in f]
+    print(stroke_name)
+    print("stroke num:", len(stroke_name))
+
+    stroke_map = {}
+
+    type_img_names = []
+
+    for s in stroke_name:
+        path = os.path.join(type_path, s)
+        stroke_imgs = [f for f in os.listdir(path) if '.png' in f]
+        for img in stroke_imgs:
+            stroke_map[img] = s
+            type_img_names.append(img)
+
+    print(len(stroke_map))
+    print(len(type_img_names))
+
+    all_imgs_path = '/Users/liupeng/Documents/Data/stroke_classification_dataset/images'
+    all_names = [f for f in os.listdir(all_imgs_path) if '.png' in f]
+    print(len(all_names))
+
+    img_names_diff = []
+    for s in all_names:
+        if s not in type_img_names:
+            print(s)
+            img_names_diff.append(s)
+    print(len(img_names_diff))
+
+    # img_diff_path = '/Users/liupeng/Documents/Data/stroke_classification_dataset/img_diff'
+
+    # for s in img_names_diff:
+    #     shutil.copy2(os.path.join(all_imgs_path, s), img_diff_path)
+
+    xml_path = "../../../Data/Characters/radical_add_stroke_position_similar_structure.xml"
+    save_path = "../../../Data/Characters/radical_add_stroke_position_similar_structure_add_stroke_order.xml"
+
+    tree = ET.parse(xml_path)
+    if tree is None:
+        print("tree is none!")
+        return
+    root = tree.getroot()
+    print("root len:", len(root))
+
+    for i in range(len(root)):
+        radical = root[i]
+
+        if len(radical.attrib['TAG']) <= 0 or len(radical.attrib['TAG']) > 1:
+            continue
+        tag = radical.attrib['TAG']
+        # find all images
+        imgs = []
+        for s in type_img_names:
+            if tag + '_' in s:
+                imgs.append(s)
+        # sort images
+        max_num = 0
+        for s in imgs:
+            s = s.replace('.png', '')
+            ss = s.split('_')
+            num = int(ss[2].replace(' ', ''))
+            max_num = max(num, max_num)
+
+        imgs_sorted = []
+        for k in range(max_num+1):
+            for s in imgs:
+                if '_' + str(k) + '.png' in s:
+                    imgs_sorted.append(s)
+        stroke_order = []
+        for s in imgs_sorted:
+            stroke_order.append(stroke_map[s])
+        stroke_order_str = ''
+        for j in range(len(stroke_order)):
+            if j == len(stroke_order) - 1:
+                stroke_order_str += stroke_order[j]
+            else:
+                stroke_order_str += stroke_order[j] + '|'
+
+        stroke_order_elem = ET.Element("STROKE_ORDER")
+        stroke_order_elem.text = stroke_order_str
+
+        root[i].append(stroke_order_elem)
+
+    prettyXml(root, '\t', '\n')
+    tree.write(save_path, encoding='utf-8')
+
+
+
+
+if __name__ == '__main__':
+    add_stroke_order_to_XML()
--- utils/Functions.py
+++ utils/Functions.py
@@ -8,6 +8,23 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
+def prettyXml(element, indent, newline, level = 0): # elemnt为传进来的Elment类，参数indent用于缩进，newline用于换行
+    if element:  # 判断element是否有子元素
+        if element.text == None or element.text.isspace(): # 如果element的text没有内容
+            element.text = newline + indent * (level + 1)
+        else:
+            element.text = newline + indent * (level + 1) + element.text.strip() + newline + indent * (level + 1)
+    #else:  # 此处两行如果把注释去掉，Element的text也会另起一行
+        #element.text = newline + indent * (level + 1) + element.text.strip() + newline + indent * level
+    temp = list(element) # 将elemnt转成list
+    for subelement in temp:
+        if temp.index(subelement) < (len(temp) - 1): # 如果不是list的最后一个元素，说明下一个行是同级别元素的起始，缩进应一致
+            subelement.tail = newline + indent * (level + 1)
+        else:  # 如果是list的最后一个元素， 说明下一行是母元素的结束，缩进应该少一个
+            subelement.tail = newline + indent * level
+        prettyXml(subelement, indent, newline, level = level + 1) # 对子元素进行递归操作
+
+
 
 def removeShortBranchesOfSkeleton(skeleton, length_threshold=30):
     """
